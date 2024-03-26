bdd7fbcaa90fa382dfe098a5b619236e4d7a5d83
valerian <valerianvdc.pro@gmail.com>
2024-03-26 14:56:17 +0100

    test gps

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ window.onload = () => {
     let testEntityAdded = false;
 
     const el = document.querySelector("[gps-new-camera]");
+    const desiredLatitude = 50.4758555; // Example latitude
+    const desiredLongitude = 4.4724386; // Example longitude
 
     el.addEventListener("gps-camera-update-position", e => {
         if(!testEntityAdded) {
-            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+            alert(`Got first GPS position: lon ${desiredLongitude} lat ${desiredLatitude}`);
             // Add a box to the north of the initial GPS position
             const entity = document.createElement("a-box");
             entity.setAttribute("scale", {
